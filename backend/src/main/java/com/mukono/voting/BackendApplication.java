73aad71386dd8d9c6bbed9336dab09e2f0d3af34
Simon Fred Lubambo <slubambo57@gmail.com>
2026-01-28 17:21:23 +0300

    Remove Lombok configuration and simplify Spring Boot annotations
    Eliminated Lombok-related configurations from `pom.xml` and removed redundant `@EnableJpaRepositories` and `@EntityScan` annotations in `BackendApplication` to streamline application setup.

--- backend/src/main/java/com/mukono/voting/BackendApplication.java
+++ backend/src/main/java/com/mukono/voting/BackendApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = "com.mukono.voting")
-@EnableJpaRepositories(basePackages = "com.mukono.voting")
-@EntityScan(basePackages = "com.mukono.voting")
+@SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
